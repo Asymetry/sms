847b927d6f60626fa21b85844f27ada0466782d2
Asymetry <59914777+Asymetry@users.noreply.github.com>
2020-02-10 22:50:53 +0300

    Update Asymetry.py

--- Asymetry.py
+++ Asymetry.py
@@ -23,42 +23,41 @@ print("""
 
 """)
 
-islemno = raw_input("\033[91mAracınızın numarasını girin ==> ");
+islemno = raw_input("\033[91mAracınızın numarasını girin ==> ")
 
 
 
 if(islemno == "1"):
-	os.system("clear");
-        os.system("figlet Asymetry");
-	hedefip = raw_input("\033[96mHedef İP Girin ==> ");
-        os.system("nmap -sS -sV -O " + hedefip);
+	os.system("clear")
+        os.system("figlet Asymetry")
+	hedefip = raw_input("\033[96mHedef İP Girin ==> ")
+        os.system("nmap -sS -sV -O " + hedefip)
 
 elif(islemno == "2"):
-	os.system("clear");
-        os.system("figlet Asymetry");
-	kelime = raw_input("\033[96mKelimenizi girin ==> ");
-	os.system("searchsploit " + kelime);
+	os.system("clear")
+        os.system("figlet Asymetry")
+	kelime = raw_input("\033[96mKelimenizi girin ==> ")
+	os.system("searchsploit " + kelime)
 
 elif(islemno == "3"):
-	os.system("clear");
-        os.system("figlet Asymetry");
-	os.system("ifconfig eth0 down");
-	os.system("macchanger -r eth0");
-	os.system("ifconfig eth0 up");
-	print("\033[96mYeni MAC Adresi random belirlendi.");
+	os.system("clear")
+        os.system("figlet Asymetry")
+	os.system("ifconfig eth0 down")
+	os.system("macchanger -r eth0")
+	os.system("ifconfig eth0 up")
+	print("\033[96mYeni MAC Adresi random belirlendi.")
 
 
 elif(islemno == "4"):
-	os.system("ifconfig eth0 down");
-	os.system("macchanger -p eth0");
-	os.system("ifconfig eth0 up");
-	print("\033[96mMAC adresiniz orjinaline döndürüldü.");  
+	os.system("ifconfig eth0 down")
+	os.system("macchanger -p eth0")
+	os.system("ifconfig eth0 up")
+	print("\033[96mMAC adresiniz orjinaline döndürüldü.")
 
-istek = raw_input("Aracımız yeniden başlatılsınmı.( e ): ");
+istek = raw_input("Aracımız yeniden başlatılsınmı.( e ): ")
 
 if(istek == "e"):
 	os.system("python Asymetry.py")
 
-else:
-	os.system("python Asymetry.py");
+
 
