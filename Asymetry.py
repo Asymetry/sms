7dd836126f1d1e1040f008b8780d35251e03d124
Asymetry <59914777+Asymetry@users.noreply.github.com>
2020-05-09 22:57:34 +0000

    Update Asymetry.py

--- Asymetry.py
+++ Asymetry.py
@@ -3,61 +3,88 @@
 
 import os
 
-os.system("apt-get install figlet")
-os.system("apt-get install searchsploit")
 os.system("clear")
-os.system("figlet Asymetry")
-os.system
 
-print("""
+print("""\033[96m
+    \                              |              
+   _ \    __| |   | __ `__ \   _ \ __|  __| |   | 
+  ___ \ \__ \ |   | |   |   |  __/ |   |    |   | 
+_/    _\____/\__, |_|  _|  _|\___|\__|_|   \__, | 
+             ____/                         ____/  
 
 ##################################                                 
-         * ASYMETRY *
 
 1-) Port tarama
 2-) Exploit arama 
 3-) MAC Adresi değiştirme 
-4-) MAC geri getirme
 
 ###################################
 
 """)
 
-islemno = raw_input("\033[91mAracınızın numarasını girin ==> ")
+islemno = raw_input("\033[96mAracınızın numarasını girin ==> ")
 
 
 
 if(islemno == "1"):
-	os.system("clear")
-        os.system("figlet Asymetry")
+	
 	hedefip = raw_input("\033[96mHedef İP Girin ==> ")
         os.system("nmap -sS -sV -O " + hedefip)
 
-elif(islemno == "2"):
-	os.system("clear")
-        os.system("figlet Asymetry")
+
+if(islemno == "2"):
+	
 	kelime = raw_input("\033[96mKelimenizi girin ==> ")
 	os.system("searchsploit " + kelime)
 
-elif(islemno == "3"):
-	os.system("clear")
-        os.system("figlet Asymetry")
+if(islemno == "3"):
+	
+	print("""
+
+  İnternete koblolu olarakmı yani eth0 olarakmı bağlanıyorsunuz yada
+ kablosuz yani wlan0 olarakmı seçiniz. 
+
+1-)eth0
+2-)wlan0
+3-)Mac adresini geri getirme
+""")
+	islem = raw_input("==> ")
+if(islem == "1"):
+	
 	os.system("ifconfig eth0 down")
 	os.system("macchanger -r eth0")
 	os.system("ifconfig eth0 up")
-	print("\033[96mYeni MAC Adresi random belirlendi.")
 
+if(islem == "2"):
 
-elif(islemno == "4"):
+	os.system("ifconfig wlan0 down")
+	os.system("macchanger -r wlan0")
+	os.system("ifconfig wlan0 up")
+
+if(islem == "3"):
+
+	print("""
+1-)eth0
+2-)wlan0
+""")
+	sec = raw_input("==> ")
+if(sec == "1"):
 	os.system("ifconfig eth0 down")
 	os.system("macchanger -p eth0")
 	os.system("ifconfig eth0 up")
-	print("\033[96mMAC adresiniz orjinaline döndürüldü.")
+elif(sec == "2"):
+	os.system("ifconfig wlan0 down")
+	os.system("macchanger -p wlan0")
+	os.system("ifconfig wlan0 up")		
 
-istek = raw_input("Aracımız yeniden başlatılsınmı.( e ): ")
 
-if(istek == "e"):
-	os.system("python Asymetry.py")
+
+
+
+
+
+
+
 
 
 
