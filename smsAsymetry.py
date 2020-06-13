fe75a17e1b02d95b0b340ed737fdcc58b934b4f5
Asymetry <59914777+Asymetry@users.noreply.github.com>
2020-06-13 18:00:51 +0300

    Update and rename smsAsymetry to smsAsymetry.py

---
+++ smsAsymetry.py
@@ -0,0 +1,37 @@
+import requests
+from os import system as s
+
+banner = """
+
+| İstediginiz telefon adresine hergun 1 defa mesaj atma hakkınız vardır 
+| Mesajınız karakter sayısı sınırlı bunu mesajınızı yazdıktan sonra görüceksiniz.
+| Tel adresinizi Doğru girmezseniz hata alabilrsiniz.
+
+"""
+
+print(banner)
+
+sor = input("Tel adresi Örn:05555555555 >>> ")
+
+mesaj = input("Mesajınız >>> ")
+
+arlk = mesaj[0:70]
+
+print("\n| Mesajınızın Gönderilebilecek kısmı aşagıdaki gibidir.\n"+arlk)
+
+drlm = input("\n| Mesajınız Gönderilsinmi?[y/n] >>> ")
+
+if drlm == "y" or drlm == "Y":
+    print("\n"+sor+"\n"+arlk+"\n")
+    resp = requests.post('https://textbelt.com/text', {
+  'phone': sor,
+  'message': arlk,
+  'key': 'textbelt',
+    })
+    print(resp.json())
+
+elif drlm == "n" or drlm == "N":
+    s("python3 dnme.py")
+else:
+    print("\n|Hata yaptınız.")
+
